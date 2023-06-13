73a61507c34b1787b0d102541de7a9fbcff75b55
anton-tchaikovsky <106504887+anton-tchaikovsky@users.noreply.github.com>
2023-06-13 08:06:33 +0300

    Сделал приложение по методичке.

---
+++ app/src/main/java/com/example/firsttests/domain/EmailValidator.kt
@@ -0,0 +1,40 @@
+package com.example.firsttests.domain
+
+import android.text.Editable
+import android.text.TextWatcher
+
+import java.util.regex.Pattern
+
+class EmailValidator : TextWatcher {
+
+    internal var isValid = false
+
+    override fun afterTextChanged(editableText: Editable) {
+        isValid = isValidEmail(editableText)
+    }
+
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) = Unit
+
+    internal fun isValidEmail(email: CharSequence?): Boolean {
+        return email != null && EMAIL_PATTERN.matcher(email).matches()
+    }
+
+    companion object {
+
+        /**
+         * Паттерн для сравнения.
+         */
+        private val EMAIL_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9+._%\\-]{1,256}" +
+                    "@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+        )
+
+    }
+}
